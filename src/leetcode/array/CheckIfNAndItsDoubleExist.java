9f9bb9a8d88240ffb2ff8d3b1ddf534f7b821399
anpopo <dkstpgud@gmail.com>
2021-10-27 23:49:41 +0900

    boj basic math

--- src/leetcode/array/CheckIfNAndItsDoubleExist.java
+++ src/leetcode/array/CheckIfNAndItsDoubleExist.java
@@ -1,2 +1,13 @@
-package leetcode.array;public class CheckIfNandItsDoubleExist {
+package leetcode.array;
+
+public class CheckIfNAndItsDoubleExist {
+    public boolean checkIfExist(int[] arr) {
+        for(int j = 0; j < arr.length; j++) {
+            for(int i = 0; i < arr.length; i++) {
+                if(i == j) continue;
+                if(arr[j] == arr[i] * 2) return true;
+            }
+        }
+        return false;
+    }
 }

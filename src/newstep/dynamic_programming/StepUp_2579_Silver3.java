98bc82eab264cab5e049332b44723d36cefa2e2e
ansehyeong <dkstpgud@gmail.com>
2022-11-16 21:28:50 +0900

    # class 3 - 동적 프로그래밍 큰 문제와 작은 문제가 다른 경우

--- src/newstep/dynamic_programming/StepUp_2579_Silver3.java
+++ src/newstep/dynamic_programming/StepUp_2579_Silver3.java
@@ -5,33 +5,31 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class StepUp_2579_Silver3 {
-    private static int[] dx;
-    private static int[] dy;
-    private static int[] arr;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        dx = new int[N + 1];
-        dy = new int[N + 1];
-
-        arr = new int[N + 1];
-        for (int i = 1;i <= N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+        int[] score = new int[N + 1];
+        for (int i = 1;i <= N;i ++) {
+            score[i] = Integer.parseInt(br.readLine());
         }
 
-        dx[1] = 1;
-        dx[2] = 1;
-        dy[2] = 1;
+        // 0 - 1칸을 밟고 올라오는 합계
+        // 1 - 2칸을 밟고 올라오는 합계
+        int[][] dy = new int[N + 1][2];
 
-        for (int i = 3;i <= N; i++) {
-            dx[i] = dy[i - 1];
-            dy[i] = dx[i - 2] + dy[i - 2];
+        dy[1][0] = score[1];
+        if (N > 1) {
+            dy[2][0] = score[1] + score[2];
+            dy[2][1] = score[2];
         }
 
-        System.out.println();
+        for (int i = 3; i <= N; i++) {
+            dy[i][0] = dy[i - 1][1] + score[i];
+            dy[i][1] = Math.max(dy[i - 2][0] + score[i], dy[i - 2][1] + score[i]);
+        }
 
+        System.out.println(Math.max(dy[N][0], dy[N][1]));
     }
 }

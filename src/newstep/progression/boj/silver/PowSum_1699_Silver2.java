6582c3cb1f1bca1e87dc7414906db9c1d6a0d7be
ansehyeong <dkstpgud@gmail.com>
2022-12-01 09:24:05 +0900

    feat: 동적 프로그래밍 연습

---
+++ src/newstep/progression/boj/silver/PowSum_1699_Silver2.java
@@ -0,0 +1,27 @@
+package newstep.progression.boj.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PowSum_1699_Silver2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dy = new int[N + 1];
+        dy[1] = 1;
+
+        int min;
+        for (int i = 2; i <= N; i++) {
+            min = Integer.MAX_VALUE;
+
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dy[i - j * j]);
+            }
+
+            dy[i] = min + 1;
+        }
+        System.out.println(dy[N]);
+
+    }
+}

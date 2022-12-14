ea6c015aeb368c216fabf5feef406b1c2d8ee8c0
ansehyeong <dkstpgud@gmail.com>
2022-12-15 08:58:43 +0900

    feaf: 입출력 문제 연습

---
+++ src/newstep/progression/boj/bronze/JustPrint_11719_Bronze2.java
@@ -0,0 +1,15 @@
+package newstep.progression.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JustPrint_11719_Bronze2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s;
+        while ((s = br.readLine()) != null) {
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/newstep/progression/boj/bronze/StringLength_2743_Bronze5.java
@@ -0,0 +1,14 @@
+package newstep.progression.boj.bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StringLength_2743_Bronze5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        System.out.println(s.length());
+    }
+}

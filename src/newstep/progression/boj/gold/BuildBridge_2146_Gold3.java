06ddfb3b350a53d82fd481ac0e3ab5804789d211
ansehyeong <dkstpgud@gmail.com>
2023-01-09 17:14:08 +0900

    fix : 다리만들기 dfs 와 bfs 조합 , 속도 개선 없음...ㅠㅠ

--- src/newstep/progression/boj/gold/BuildBridge_2146_Gold3.java
+++ src/newstep/progression/boj/gold/BuildBridge_2146_Gold3.java
@@ -3,6 +3,7 @@ package newstep.progression.boj.gold;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
@@ -10,8 +11,9 @@ import java.util.StringTokenizer;
 public class BuildBridge_2146_Gold3 {
 
     private static int N;
+    private static int islandNum = 2;
     private static int[][] map;
-    private static int[][] island;
+    private static boolean[][] visited;
     private static int[][] dir = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     private static int ans = Integer.MAX_VALUE;
 
@@ -19,7 +21,6 @@ public class BuildBridge_2146_Gold3 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         map = new int[N + 1][N + 1];
-        island = new int[N + 1][N + 1];
 
         for (int i = 1; i <= N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -39,9 +40,9 @@ public class BuildBridge_2146_Gold3 {
     private static void bridgeBuilder() {
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                int islandNum = island[i][j];
+                int islandNum = map[i][j];
                 if (islandNum != 0) {
-                    map = new int[N + 1][N + 1];
+                    visited = new boolean[N + 1][N + 1];
                     bfs(i, j, islandNum);
                 }
             }
@@ -50,8 +51,8 @@ public class BuildBridge_2146_Gold3 {
 
     private static void bfs(int x, int y, int islandNum) {
         Queue<Integer[]> q = new LinkedList<>();
-        q.offer(new Integer[]{x, y, 1});
-        map[x][y] = 1;
+        q.offer(new Integer[]{x, y, 0});
+        visited[x][y] = true;
 
         while (!q.isEmpty()) {
             Integer[] point = q.poll();
@@ -64,14 +65,14 @@ public class BuildBridge_2146_Gold3 {
                     continue;
                 }
 
-                if (island[nx][ny] == islandNum || map[nx][ny] != 0) {
+                if (map[nx][ny] == islandNum || visited[nx][ny]) {
                     continue;
                 }
-                map[nx][ny] = 1;
+                visited[nx][ny] = true;
 
-                if (island[nx][ny] == 0) {
+                if (map[nx][ny] == 0) {
                     q.offer(new Integer[]{nx, ny, point[2] + 1});
-                } else if (island[nx][ny] != islandNum) {
+                } else if (map[nx][ny] != islandNum) {
                     ans = Math.min(point[2], ans);
                 }
             }
@@ -79,40 +80,30 @@ public class BuildBridge_2146_Gold3 {
     }
 
     private static void islandSeparate() {
-        int cnt = 1;
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                if (map[i][j] == 1 && island[i][j] == 0) {
-                    islandBfs(i, j, cnt);
-                    cnt++;
+                if (map[i][j] == 1) {
+                    islandDfs(i, j);
+                    islandNum++;
                 }
             }
         }
     }
 
-    private static void islandBfs(int x, int y, int cnt) {
-        Queue<Integer[]> q = new LinkedList<>();
-        island[x][y] = cnt;
-        q.offer(new Integer[]{x, y});
+    private static void islandDfs(int x, int y) {
+        map[x][y] = islandNum;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dir[i][0];
+            int ny = y + dir[i][1];
 
-        while (!q.isEmpty()) {
-            Integer[] point = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = point[0] + dir[i][0];
-                int ny = point[1] + dir[i][1];
-
-                if (nx < 1 || nx > N || ny < 1 || ny > N) {
-                    continue;
-                }
-
-                if (map[nx][ny] != 1 || island[nx][ny] != 0) {
-                    continue;
-                }
-
-                island[nx][ny] = cnt;
-                q.offer(new Integer[]{nx, ny});
+            if (nx < 1 || nx > N || ny < 1 || ny > N) {
+                continue;
             }
+
+            if (map[nx][ny] != 1) {
+                continue;
+            }
+            islandDfs(nx, ny);
         }
     }
 
